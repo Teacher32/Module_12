cb2904d3d93df003da93ec5a7129d52b0c6db7d3
Teacher32 <kitaev-yuri@yandex.ru>
2021-04-24 22:09:01 +0300

    Добавлены дополнительные комментарии

--- src/mod12/stock_market/Main.java
+++ src/mod12/stock_market/Main.java
@@ -6,13 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         StockAccount stockAccount = new StockAccount(); // заводим счет на бирже
-        StockAccount.start(); // запускаем счет в работу
+        StockAccount.start(); // запускаем счет в работу    // ПОЧЕМУ КРАСНОЕ ПОДЧЕРКИВАНИЕ?
         long profit = 0;        // переменная прибыли
         // далее блок управления с вводом данных от пользователя
         Scanner scanner = new Scanner(System.in);
         String command = "";    // начальное значение переменной, которая хранит введенные пользователем данные
         while(!command.equals("exit")) {    // пока не будет введена команда "exit", выполнять switch-case
-            command = in.next();
+            command = in.next();    // ПОЧЕМУ ТУТ ОШИБКА????
             switch(command) {
                 case "check":
                     System.out.println(stockAccount.money);
